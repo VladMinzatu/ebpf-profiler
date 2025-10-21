cfff4d60bc9bfeafaa37de210be8188714cdcb54
Vlad Minzatu <minzatu.vlad@gmail.com>
2025-10-21 16:59:37 +0000

    add profiler abstraction

---
+++ internal/profiler/profiler.go
@@ -0,0 +1,145 @@
+package profiler
+
+import (
+	"context"
+	"errors"
+	"log/slog"
+	"sync"
+	"time"
+)
+
+type EbpfBackend interface {
+	Start(targetPID int, samplingPeriodNs uint64) error
+	Stop() error
+	SnapshotCounts() (map[uint64]uint64, error)
+	LookupStacks(userID uint32, kernID uint32) ([]uint64, []uint64, error)
+}
+
+type Sample struct {
+	Timestamp   time.Time
+	UserStack   []uint64
+	KernelStack []uint64
+	Count       uint64
+}
+
+type Profiler struct {
+	pid             int
+	sampleHz        int
+	collectInterval time.Duration
+	backend         EbpfBackend
+
+	samplesCh chan []Sample
+
+	started bool
+	mu      sync.Mutex
+	ctx     context.Context
+	cancel  context.CancelFunc
+	wg      sync.WaitGroup
+}
+
+func NewProfiler(pid int, sampleHz int, collectInterval time.Duration, backend EbpfBackend) (*Profiler, error) {
+	if collectInterval <= 1*time.Millisecond {
+		return nil, errors.New("invalid collectInterval; must be > 1ms")
+	}
+	if sampleHz <= 0 {
+		return nil, errors.New("invalid sampleHz; must be > 0")
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	return &Profiler{pid: pid,
+		sampleHz:        sampleHz,
+		collectInterval: collectInterval,
+		backend:         backend,
+		ctx:             ctx,
+		cancel:          cancel,
+		samplesCh:       make(chan []Sample, 1),
+	}, nil
+}
+
+func (p *Profiler) Samples() <-chan []Sample { return p.samplesCh }
+
+func (p *Profiler) Start() error {
+	p.mu.Lock()
+	if p.started {
+		p.mu.Unlock()
+		return errors.New("profiler already started")
+	}
+	p.started = true
+	p.mu.Unlock()
+
+	periodNs := uint64(1_000_000_000 / p.sampleHz)
+	if err := p.backend.Start(p.pid, periodNs); err != nil {
+		p.mu.Lock()
+		p.started = false
+		p.mu.Unlock()
+		return err
+	}
+
+	p.wg.Add(1)
+	go p.collector()
+
+	return nil
+}
+
+func (p *Profiler) Stop() error {
+	var stopErr error
+	p.cancel()
+
+	if err := p.backend.Stop(); err != nil {
+		stopErr = err
+	}
+
+	// Wait for collector to exit
+	p.wg.Wait()
+	close(p.samplesCh)
+
+	p.mu.Lock()
+	p.started = false
+	p.mu.Unlock()
+	return stopErr
+}
+
+func (p *Profiler) collector() {
+	defer p.wg.Done()
+
+	ticker := time.NewTicker(p.collectInterval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-p.ctx.Done():
+			return
+		case t := <-ticker.C:
+			counts, err := p.backend.SnapshotCounts()
+			if err != nil {
+				slog.Warn("Failed to collect counts from ebpf map", "error", err)
+				continue
+			}
+
+			var samples []Sample
+			for key, cnt := range counts {
+				userID, kernID := unpackKey(key)
+
+				userStack, kernStack, err := p.backend.LookupStacks(userID, kernID)
+				if err != nil {
+					slog.Warn("Failed to resolve stack keys", "error", err)
+					continue
+				}
+
+				s := Sample{
+					Timestamp:   t,
+					UserStack:   userStack,
+					KernelStack: kernStack,
+					Count:       cnt,
+				}
+				samples = append(samples, s)
+			}
+
+			select {
+			case p.samplesCh <- samples:
+			default:
+				slog.Warn("consumer wasn't ready, sample dropped")
+			}
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,7 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/VladMinzatu/ebpf-profiler/internal/profiler"
+)
 
 func main() {
-	fmt.Println("Let's get the ball rolling")
+	backend, err := profiler.NewEbpfProfiler()
+	if err != nil {
+		slog.Error("Failed to initialise ebpf backend", "error", err)
+		os.Exit(1)
+	}
+	p, err := profiler.NewProfiler(os.Getegid(), 1000, 1*time.Second, backend)
+	if err != nil {
+		slog.Error("Failed to initialise profiler", "error", err)
+		os.Exit(1)
+	}
+
+	err = p.Start()
+	if err != nil {
+		slog.Error("Failed to start profiler", "error", err)
+		os.Exit(1)
+	}
+	defer p.Stop()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		samples := p.Samples()
+		for s := range samples {
+			fmt.Println(s)
+		}
+	}()
+
+	go func() {
+		done := time.Now().Add(10 * time.Second)
+		for time.Now().Before(done) {
+			hotCaller()
+		}
+	}()
+
+	<-stop
+}
+
+//go:noinline
+func hotFunc() {
+	for i := 0; i < 1000; i++ {
+		_ = i * i
+	}
+}
+
+//go:noinline
+func hotCaller() {
+	hotFunc()
 }
